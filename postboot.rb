a0b1e4e4fa3fd7c389ad9857740321599b2166c8
Traxmaxx <github@netzok.net>
2012-11-27 18:17:30 +0100

    Added elephantsql detection if postgresql is defined

--- postboot.rb
+++ postboot.rb
@@ -1,4 +1,4 @@
-module MyApp
+module Cloudcontrol
   class Application < Rails::Application
 
     def config.database_configuration
@@ -19,8 +19,25 @@ module MyApp
               end
             end
           end
-      end
 
+        when 'postgresql' # postgres
+          config[rails_env].each do |key, value|
+            if value === nil && ENV["ELEPHANTSQL_URL"] # get value from cctrl ENV if nil and elephantsql added
+              elephant_uri = URI.parse(ENV["ELEPHANTSQL_URL"])
+              if key === 'database'
+                config[rails_env][key] = elephant_uri.path[1..-1]
+              elsif key === 'username'
+                config[rails_env][key] = elephant_uri.user
+              elsif key === 'password'
+                config[rails_env][key] = elephant_uri.password
+              elsif key === 'host'
+                config[rails_env][key] = elephant_uri.host
+              elsif key === 'port'
+                config[rails_env][key] = elephant_uri.port
+              end
+            end
+          end
+      end
       return config
     end
   end
